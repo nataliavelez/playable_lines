7263747f7f1bfa038cf38dc19857a69cb6d93030
rcalcott9 <rachel.calcott9@gmail.com>
2024-06-05 17:20:02 -0400

    Revert "remove duplicate images"
    This reverts commit 5500ecb1ae9ec0e814ddf23ced85ef8cb27abaf3.

--- client/src/stages/game/scenes/Preloader.js
+++ client/src/stages/game/scenes/Preloader.js
@@ -40,10 +40,11 @@ export class Preloader extends Scene
         this.load.image('Fences', 'Fences.png');
         this.load.image('Mushrooms, Flowers, Stones', 'Mushrooms, Flowers, Stones.png');
         this.load.image('Tilled_Dirt', 'Tilled_Dirt.png');
-
+        this.load.image('Fences', 'Fences.png');
+        this.load.image('Mushrooms, Flowers, Stones', 'Mushrooms, Flowers, Stones.png');
         
         this.load.tilemapTiledJSON('cloud-city-map', 'cloud_city.json'); // demo
-        this.load.tilemapTiledJSON('test-map', 'test_map_3.json');
+        this.load.tilemapTiledJSON('test-map', 'test_map_4.json');
 
         this.load.spritesheet('player', 'characters.png', {
             frameWidth: 52,
