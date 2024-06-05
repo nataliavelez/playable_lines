51ec114734149e77385a019de60708469d9bb3ca
Josh White <jpwhite@mit.edu>
2024-06-05 16:01:58 -0400

    Add result change

--- client/src/Stage.jsx
+++ client/src/Stage.jsx
@@ -7,6 +7,7 @@ import {
 import { Loading } from "@empirica/core/player/react";
 import React from "react";
 import { GridWorld } from "./stages/GridWorld";
+import { Result } from "./stages/Result";
 
 export function Stage() {
   const player = usePlayer();
@@ -14,21 +15,12 @@ export function Stage() {
   const round = useRound();
   const stage = useStage();
 
-  if (player.stage.get("submit")) {
-    if (players.length === 1) {
-      return <Loading />;
-    }
-
-    return (
-      <div className="text-center text-gray-400 pointer-events-none">
-        Please wait for other player(s).
-      </div>
-    );
-  }
 
   switch (stage.get("name")) {
-    case "Grid-Engine Example":
+    case "game":
       return <GridWorld />;
+    case "result":
+      return <Result />;
     default:
       return <Loading />;
   }
---
+++ client/src/stages/Result.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { usePlayer, usePlayers } from "@empirica/core/player/classic/react";
+import { Button } from "../components/Button";
+
+export function Result() {
+  const player = usePlayer();
+
+  return (
+    <div>
+      <p>You got X amount of water!</p>
+      <p>This was more than X players, the same as Y players, and less than Z players</p>
+      <br />
+      <p>You earned X dollars</p>
+      <p>Press the button to condition</p>
+
+     {/* <Button handleClick={() => player.stage.set("submit", true)}>
+        Continue
+      </Button>
+      */}
+    </div>
+  );
+  // const players = usePlayers();
+  // const partner = players.filter((p) => p.id !== player.id)[0];
+
+  /*
+  return (
+    <div>
+      <p>You chose: {player.round.get("decision")}</p>
+      <p>Your partner chose: {partner.round.get("decision")}</p>
+      <br />
+      <p>You get {player.round.get("score") || "TBD"} months in jail!</p>
+
+      <Button handleClick={() => player.stage.set("submit", true)}>
+        Continue
+      </Button>
+    </div>
+  );*/
+}
