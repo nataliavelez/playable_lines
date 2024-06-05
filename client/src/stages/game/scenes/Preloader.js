7d9c1556387d87c222805b3d34cbe5221f7b4cfa
rcalcott9 <rachel.calcott9@gmail.com>
2024-06-05 17:20:11 -0400

    Revert "more images"
    This reverts commit 5f97378dc0a8ab8bfbadb51cd433f40a8240dede.

--- client/src/stages/game/scenes/Preloader.js
+++ client/src/stages/game/scenes/Preloader.js
@@ -44,7 +44,7 @@ export class Preloader extends Scene
         this.load.image('Mushrooms, Flowers, Stones', 'Mushrooms, Flowers, Stones.png');
         
         this.load.tilemapTiledJSON('cloud-city-map', 'cloud_city.json'); // demo
-        this.load.tilemapTiledJSON('test-map', 'test_map_4.json');
+        this.load.tilemapTiledJSON('test-map', 'test_map_3.json');
 
         this.load.spritesheet('player', 'characters.png', {
             frameWidth: 52,
