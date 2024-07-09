a58f91b9ee153784de98e075bcdb6e56bb0a9cec
Josh White <jpwhite@mit.edu>
2024-07-09 19:59:56 -0400

    Add cumulative score to profile (instead of avatar)

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -13,6 +13,7 @@ export function Profile() {
   const stage = useStage();
 
   const score = player.round.get("score") || 0;
+  const cumScore = player.get("cumScore") + score; 
 
   return (
     <div className="min-w-lg md:min-w-2xl mt-2 m-x-auto px-3 py-2 text-gray-500 rounded-md grid grid-cols-3 items-center border-.5">
@@ -30,14 +31,19 @@ export function Profile() {
       <div className="flex space-x-3 items-center justify-end">
         <div className="flex flex-col items-center">
           <div className="text-xs font-semibold uppercase tracking-wide leading-none text-gray-400">
-            Score
+            Round Score
           </div>
           <div className="text-3xl font-semibold !leading-none tabular-nums">
             {score}
           </div>
         </div>
-        <div className="h-11 w-11">
-          <Avatar player={player} />
+        <div className="flex flex-col items-center">
+          <div className="text-xs font-semibold uppercase tracking-wide leading-none text-gray-400">
+            Total Score
+          </div>
+          <div className="text-3xl font-semibold !leading-none tabular-nums">
+            {cumScore}
+          </div>
         </div>
       </div>
     </div>
