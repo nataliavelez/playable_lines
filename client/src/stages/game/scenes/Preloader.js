8e1b269cdf5036bc07faa9a242bdcfe763b7f2d3
Josh White <jpwhite@mit.edu>
2024-07-09 23:36:40 -0400

    clean preloader

--- client/src/stages/game/scenes/Preloader.js
+++ client/src/stages/game/scenes/Preloader.js
@@ -1,41 +1,26 @@
 import { Scene } from 'phaser';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
-        console.log('Preloader: scene');
-
+    init() {
         const centerX = this.cameras.main.width / 2;
         const centerY = this.cameras.main.height / 2;
 
-
-        //  We loaded this image in our Boot Scene, so we can display it here
+        //  Show the background image
         this.add.image(512, 384, 'background');
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(centerX, centerY, 468, 32).setStrokeStyle(1, 0xffffff);
-
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(centerX-230, centerY, 4, 28, 0xffffff);
-
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
-
+        // Progress bar. 
+        this.add.rectangle(centerX, centerY, 468, 32).setStrokeStyle(1, 0xffffff); // bar outline.
+        const bar = this.add.rectangle(centerX-230, centerY, 4, 28, 0xffffff); //bar
+        this.load.on('progress', (progress) => { //  'progress' event emitted by the LoaderPlugin
+            bar.width = 4 + (460 * progress); //  Update progress bar (464px = 100%)
         });
     }
 
-    preload ()
-    {
-        // Load assets
+    preload() {
         this.load.setPath('assets');
 
         // Tile Images
@@ -59,11 +44,8 @@ export class Preloader extends Scene
 
     }
 
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
+    create() {
+        //  Could create global objects here that the rest of the game can use. i.e. Global animations. 
         //  Move to the main game. 
         this.scene.start('Game');
     }
