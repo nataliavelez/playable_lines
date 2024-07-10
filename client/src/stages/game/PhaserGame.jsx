fc45ec58223d878a03976e7f16d70deb2df35b8c
Josh White <jpwhite@mit.edu>
2024-07-10 01:38:50 -0400

    pass mapName to phaser

--- client/src/stages/game/PhaserGame.jsx
+++ client/src/stages/game/PhaserGame.jsx
@@ -3,7 +3,7 @@ import { forwardRef, useEffect, useLayoutEffect, useRef } from 'react';
 import StartGame from './main';
 import { EventBus } from './EventBus';
 
-export const PhaserGame = forwardRef(function PhaserGame({ currentActiveScene, playerStates, isVisible }, ref) {
+export const PhaserGame = forwardRef(function PhaserGame({ currentActiveScene, mapName, playerStates, isVisible }, ref) {
     const game = useRef();
 
     useLayoutEffect(() => {
