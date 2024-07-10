5ada916e84501b4ff5765f736d4f88ba8ff79d8f
Josh White <jpwhite@mit.edu>
2024-07-09 23:22:46 -0400

    fix preloader progress bar

--- client/src/stages/game/scenes/Preloader.js
+++ client/src/stages/game/scenes/Preloader.js
@@ -9,14 +9,20 @@ export class Preloader extends Scene
 
     init ()
     {
+        console.log('Preloader: scene');
+
+        const centerX = this.cameras.main.width / 2;
+        const centerY = this.cameras.main.height / 2;
+
+
         //  We loaded this image in our Boot Scene, so we can display it here
         this.add.image(512, 384, 'background');
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(centerX, centerY, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(centerX-230, centerY, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
@@ -29,10 +35,10 @@ export class Preloader extends Scene
 
     preload ()
     {
-        //  Load the assets for the game - Replace with your own assets
+        // Load assets
         this.load.setPath('assets');
 
-        this.load.image('tiles', 'cloud_tileset.png'); // demo
+        // Tile Images
         this.load.image('Water_1', 'Water_1.png');
         this.load.image('Grass_tiles_v2', 'Grass_tiles_v2.png');
         this.load.image('Water well', 'Water well.png');
@@ -42,9 +48,10 @@ export class Preloader extends Scene
         this.load.image('Mushrooms, Flowers, Stones', 'Mushrooms, Flowers, Stones.png');
         this.load.image('indicator', 'water_ready.png');
         
-        this.load.tilemapTiledJSON('cloud-city-map', 'cloud_city.json'); // demo
+        // Tile Maps
         this.load.tilemapTiledJSON('test-map', 'test_map.json');
 
+        // Spritesheet
         this.load.spritesheet('bunny', 'bunny_spritesheet.png', {
             frameWidth: 48,
             frameHeight: 48
@@ -57,7 +64,7 @@ export class Preloader extends Scene
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, you can define global animations here, so we can use them in other scenes.
 
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+        //  Move to the main game. 
         this.scene.start('Game');
     }
 }
