94d8f58130ffc59d2748a6f81c4f12e04094b8ad
Josh White <jpwhite@mit.edu>
2024-07-10 01:06:05 -0400

    add server side logic to set map and map positions for each round

--- server/src/callbacks.js
+++ server/src/callbacks.js
@@ -1,4 +1,5 @@
 import { ClassicListenersCollector } from "@empirica/core/admin/classic";
+import { getMapInfo } from './getMapInfo.js';
 export const Empirica = new ClassicListenersCollector();
 
 Empirica.onGameStart(({ game }) => {
@@ -6,23 +7,37 @@ Empirica.onGameStart(({ game }) => {
   const { numRounds } = treatment;
   const { playerCount } = treatment;
   const { universalizability } = treatment;
+
+  // get map info for learn and test rounds
+  const mapInfoLearn = getMapInfo(universalizability); // depends on universaliabilty condition
+  const mapNamesLearn = Object.keys(mapInfoLearn).sort(() => Math.random() - 0.5); // permuted
+  const mapInfoTest = getMapInfo("medium");
+  const mapNamesTest = Object.keys(mapInfoTest).sort(() => Math.random() - 0.5); // permuted
   
   // add rounds
   for (let i = 0; i < numRounds; i++) {
     const round = game.addRound({
       name: `Round ${i + 1}`,
     });
-  round.addStage({ name: "game", duration: 30 });  
-  round.addStage({ name: "result", duration: 30 });
+    round.addStage({ name: "game", duration: 30 });  
+    round.addStage({ name: "result", duration: 30 });
 
-  // make last round a test round with medium universalizability
-  if (i !== numRounds - 1) {
-    round.set("roundType", "learn");
-    round.set("mapUniversalizablity", universalizability);
-  } else {
-    round.set("roundType", "test");
-    round.set("mapUniversalizablity", "medium");
-  }
+    //set map name, details, and get permuted start positions
+    let startPositions; // init outside of if logic to access outside of it.
+    if (i !== numRounds - 1) {
+      round.set("roundType", "learn");
+      round.set("mapUniversalizablity", universalizability);
+      round.set("mapName", mapNamesLearn[i]);
+      startPositions = mapInfoLearn[mapNamesLearn[i]].sort(() => Math.random() - 0.5);
+    } else { 
+      round.set("roundType", "test");
+      round.set("mapUniversalizablity", "medium");
+      round.set("mapName", mapNamesTest[0]) // for now just one map for test round
+      startPositions = mapInfoLearn[mapNamesTest[0]].sort(() => Math.random() - 0.5);
+    }
+
+    // set start position for each player in each round
+    game.players.forEach((player, i) => player.set("startPos", startPositions[i]));
   }
 
   //Randomly set colours for players
@@ -31,19 +46,8 @@ Empirica.onGameStart(({ game }) => {
   const shuffledColors = colors.sort(() => Math.random() - 0.5); //permute colours array
   game.players.forEach((player, i) => player.set("color", shuffledColors[i]));
 
-  // starting positions -- for now just hardcoded,
-  //but once we have diff maps will have to have initial positions for each map
-  const startPositions = [
-    { x: 8, y: 8 },
-    { x: 10, y: 10 },
-  ];
-  const shuffledStartPositions = startPositions.sort(() => Math.random() - 0.5);
-
-  //set starting position for player and partner
-  game.players.forEach((player, i) => player.set("startPos", shuffledStartPositions[i]));
-
-  //set initial cumulative Scores
-  game.players.forEach((player) => player.set("cumScore", 0));
+  // Set cum score
+  game.players.forEach((player) => player.set("cumScore", 0)); // init cumulative Scores
 });
 
 
