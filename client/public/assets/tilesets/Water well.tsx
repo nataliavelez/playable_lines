6844dc53b5d53f90b96a45d44b6a7ea8b129e6cb
Josh White <jpwhite@mit.edu>
2024-07-11 14:25:59 -0400

    create external tilesets
    This seems to be better than embedding the tileset in each map, becuase then for each map you have to manually reset the custom properties, which is error prone and time consuming. So having external tilesets and referencing them into the map, should hopefully fix and make this easier

---
+++ client/public/assets/tilesets/Water well.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Water well" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../Water well.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="ge_alwaysTop" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="ge_alwaysTop" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="ge_collide" type="bool" value="true"/>
+   <property name="source" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="ge_collide" type="bool" value="true"/>
+   <property name="source" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
