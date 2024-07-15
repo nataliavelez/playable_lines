6dec42ccf51e1a83afb9034491b46da88a609b93
Josh White <jpwhite@mit.edu>
2024-07-14 23:51:13 -0400

    add styling to the keys

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "../components/Button";
+import './KeySymbols.css';
 
 export function Introduction({ next }) {
   return (
@@ -9,20 +10,25 @@ export function Introduction({ next }) {
       </h3>
       <div className="mt-2 mb-6">
         <p className="text-sm text-gray-500">
-You will be playing a multiplayer game with up to 8 other players. 
+          You will be playing a multiplayer game with up to 8 other players. 
           In the game you will walk around in various environments and collect water (from the ground or from a well) and use that to water nearby plants.  
           Your goal is to water as many plants as possible (you may water the same plant twice!). 
           Your remuneration for the experiment will depend on how well you do in the task. 
-          <b>For each plant you water you will receive $X.XX</b>. 
-          There will be four rounds (different environments) in the game.
+          <b>For each plant you water you will receive $X.XX</b>. There will be four rounds (different environments) in the game.
           <br /><br />
           Below, we quickly explain how to play the game (don't worry it is very simple), 
           before giving you a chance to practice by yourself (it will not yet be multiplayer).
           When you are confident with the instructions of the game, please press 'next' to proceed to the multiplayer verison of the game.   
           <br /><br />
           <b>Game play Instructions:</b>          
-          Use the arrow keys to move your character around the screen.
-          When you are near a water source press the space bar to collect water, and when you are near a plant press the space bar to water the plant. 
+          Use the arrow keys 
+            (<span className="key-styling">&larr;</span>
+             <span className="key-styling">&uarr;</span>
+             <span className="key-styling">&rarr;</span>
+             <span className="key-styling">&darr;</span>)
+          to move your character around the screen. 
+          When you are near a water source press the <span className="key-styling space-key">SPACE BAR</span> to collect water, 
+          and when you are near a plant press the <span className="key-styling space-key">SPACE BAR</span> to water the plant. 
           An animation will show when you are collecting/disposing water and when you are holding water.  
         </p>
       </div>
