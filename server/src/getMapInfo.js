40e141873f949aafc87f4abed2454d0a42f1e5b4
Josh White <jpwhite@mit.edu>
2024-08-26 10:53:41 -0400

    fix

--- server/src/getMapInfo.js
+++ server/src/getMapInfo.js
@@ -21,7 +21,7 @@ export function getMapInfo(universalizability) {
             medium1: [
                 {x: 6, y: 2}, {x: 7, y: 4}, {x: 8, y: 1}, {x: 9, y: 3},
                 {x: 5, y: 4}, {x: 7, y: 3}, {x: 8, y: 5}, {x: 8, y: 3}
-            ]
+            ],
             medium2: [
                 {x: 6, y: 2}, {x: 7, y: 4}, {x: 8, y: 1}, {x: 9, y: 3},
                 {x: 5, y: 4}, {x: 7, y: 3}, {x: 8, y: 5}, {x: 8, y: 3}
