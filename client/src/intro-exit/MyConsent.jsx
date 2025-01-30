98d6448520f5c2d760be178f3c0f8b6560462028
Josh White <jpwhite@mit.edu>
2025-01-30 18:40:51 -0500

    Add in proper consent form.

--- client/src/intro-exit/MyConsent.jsx
+++ client/src/intro-exit/MyConsent.jsx
@@ -4,8 +4,32 @@ export function MyConsent({ onConsent }) {
     return (
       <Consent
         title="Do you consent?"
-        text="It's all good, mate."
-        buttonText="OK"
+        text={
+          <div className = "text-[10.6px] text-black text-left space-y-1.1">
+            <p>
+              By participating in this study, you are contributing to research conducted by cognitive scientists in the Department of Brain and Cognitive Science at MIT.
+              There are neither specific benefits nor anticipated risks associated with participation in this study. Your participation in this study is completely voluntary, and you can withdraw at any time by simply exiting the study.
+              You may decline to answer any or all of the following questions. Choosing not to participate or withdrawing will result in no penalty.
+              Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you, and any information you provide will not be shared in association with any personally identifying information.
+            </p>
+            <p>
+              If you have questions about this research, please contact the researchers by sending an email to <a href="mailto:smlevine@mit.edu">Sydney Levine</a>.
+              These researchers will do their best to communicate with you in a timely, professional, and courteous manner.
+              If you have questions regarding your rights as a research subject, or if problems arise which you do not feel you can discuss with the researchers, please contact the MIT Institutional Review Board.
+            </p>
+            <h3></h3>
+            <p>
+             <b>Voluntary Participation:</b> Your participation in this research is voluntary. You may discontinue participation at any time during the research activity. You may print a copy of this consent form for your records.
+              By clicking the 'Yes' button below, I confirm that:
+            </p>
+            <ul className="list-disc list-inside">
+              <li>I am age 18 or older.</li>
+              <li>I have read and understand the information above.</li>
+              <li>I want to participate in this research and continue with the experiment.</li>
+            </ul>
+          </div>
+        }
+        buttonText="Yes"
         onConsent={onConsent}
       />
     );
