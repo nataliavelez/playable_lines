aec7f060099fe5e542e6dd1d9e26a16bdba0937e
Josh White <jpwhite@mit.edu>
2025-01-30 19:25:46 -0500

    add more images to instructions

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -17,7 +17,7 @@ export function Introduction({ next }) {
           In each environment, you will have <b>3 minutes</b> to water as many plants as possible (and you can water the same plant more than once).  
           <b> For each plant you water you will receive $X.XX</b>. 
           <br /><br />
-          Below, we explain how to play the game, before giving you a chance to practice by yourself.
+          Below, we explain how to play the game, and then give you a chance to practice by yourself before the game actually begins.
           When you are confident with the instructions and gameplay, please press 'next' to proceed.   
           <br /><br />
           <b>Gameplay instructions</b> <br />         
@@ -27,10 +27,13 @@ export function Introduction({ next }) {
              <span className="key-styling">&rarr;</span>
              <span className="key-styling">&darr;</span>)
           to move your character around the screen. 
-          When you are next to and facing a water source (either a well or ground water) press the <span className="key-styling space-key">SPACE BAR</span> to collect water, 
+          When you are next to and facing a water source (either a well, <img src="assets/Water well.png" alt="Water Tile" className="inline-block align-middle w-6 h-6"></img>,  or ground water, <img src="assets/Water_1.png" alt="Water Tile" className="inline-block align-middle w-6 h-6"></img>) press the <span className="key-styling space-key">SPACE BAR</span> to collect water, 
           and when you are next to and facing a plant (well, a sapling, <img src="assets/sapling.png" alt="Grass Tile" className="inline-block align-middle w-6 h-6"></img>) 
           press the <span className="key-styling space-key">SPACE BAR</span> to water it. 
-          An animation will show when you are collecting/disposing water and when you are holding water.  
+          When you successfully pick up water, you’ll see your watering can appear in your character’s hand, <img src="assets/watering_bunny.png" alt="watering bunny" className="inline-block align-middle w-8 h-8"></img>, and a sound will play.  
+          A water drop, <img src="assets/water_ready.png" alt="Water Drop" className="inline-block align-middle w-6 h-6"></img>, will be displayed above your character to remind you that you are carrying water.  
+          Another watering can animation will appear and another sound will play when you successfully water a plant.
+          When other players pick up and drop off water, a different sound will play.
         </p>
       </div>
       <div className="mb-6">
